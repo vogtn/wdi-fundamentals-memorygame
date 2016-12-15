59662bcf73031ecde18f558845b6ab2a38d928f9
vogtn <nicolasjvogt@gmail.com>
2016-12-15 12:54:03 -0800

    adding card creation logic to js file

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,15 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var gameBoard = document.getElementById('game-board');
+
+
+var createCards = function() {
+  for (var i=0; i< 4; i++){
+    var card = document.createElement('div');
+    card.className = 'card';
+    gameBoard.appendChild(card);
+  }
+}
+createCards();
